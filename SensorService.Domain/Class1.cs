2ed02f56ca172f48982d82a8dffc7328756ea235
Salvatore Allegra <salallegra@gmail.com>
2025-04-18 17:49:49 -0700

    Add vehicle event model

--- SensorService.Domain/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace SensorService.Domain
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ SensorService.Domain/Entities/VehicleEvent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SensorService.Domain.Entities
+{
+    public class VehicleEvent
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string LicensePlate { get; set; } = string.Empty;
+        public string SensorId { get; set; } = string.Empty;
+        public DateTime Timestamp { get; set; }
+        public string EventType { get; set; } = "Entry"; // or "Exit"
+    }
+}
